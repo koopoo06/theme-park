5b3fd273d7409b4c0ffe2422126b1590d7242d04
kopo06 <hennifer1111@gmail.com>
2023-04-24 10:28:21 +0900

    +주석, +cnt함수

--- Cal.java
+++ Cal.java
@@ -2,41 +2,69 @@ package Practice;
 import java.util.Calendar;
 
 public class Cal {
-	
-	public class Const {
-		// 권종별 가격
-	final static int 
-		BABY_PRICE = 0,
-		ADULT_DAY_PRICE = 56000, ADULT_NIGHT_PRICE = 46000,
-		TEEN_DAY_PRICE = 47000, TEEN_NIGHT_PRICE = 40000,
-		CHILD_DAY_PRICE = 44000, CHILD_NIGHT_PRICE = 37000,
-		OLD_DAY_PRICE = 44000, OLD_NIGHT_PRICE = 37000;
-	//권종별 가격 2
+
+public class Const { //final 변수들 선언
+	// 권종별 가격
 	final static int PRICE_ARRAY[][] = {{0,0,44000,47000,56000,44000},{0,0,37000,40000,46000,37000}};
-	//	 주민등록번호 분석
-	final static long FULL_DIGIT = 10000000000000L,
-			FULL_DIGIT_MIN = 10000000000L, 
-			SEVEN_DIGIT = 1000000L;
-	final static int TWO_DIGIT = 100, ONE_DIGIT = 10,
-			OLD_GENERATION = 1900, NEW_GENERATION = 2000,
-			MALE_OLD = 1, FEMALE_OLD = 2, MALE_NEW = 3, FEMALE_NEW = 4,
-			BEFORE_BIRTH = 2, AFTER_BIRTH = 1;
-	//     나이에 따른 범위
-	final static int MIN_BABY = 1, MIN_CHILD = 3, MIN_TEEN = 13, MIN_ADULT = 19,
-			MAX_CHILD = 12, MAX_TEEN = 18, MAX_ADULT = 64;
+	
+	//나이 구하기
+	final static int 
+	OLD_GENERATION = 1900, 
+	NEW_GENERATION = 2000,
+	MALE_OLD = 1, 
+	FEMALE_OLD = 2, 
+	MALE_NEW = 3, 
+	FEMALE_NEW = 4,
+	BEFORE_BIRTH = 2, 
+	AFTER_BIRTH = 1;
+	
+		
+	//그룹별 나이
+	final static int 
+	MIN_BABY = 1, 
+	MIN_CHILD = 3, 
+	MIN_TEEN = 13, 
+	MIN_ADULT = 19,
+	MAX_CHILD = 12, 
+	MAX_TEEN = 18, 
+	MAX_ADULT = 64;
+	
 	// 나이에 따른 그룹
-	final static int BABY = 1, CHILD = 2, TEEN = 3, ADULT = 4, OLD = 5;
+	final static int 
+	BABY = 1, 
+	CHILD = 2, 
+	TEEN = 3, 
+	ADULT = 4, 
+	OLD = 5;
+	
+	
 	// 할인율
-	final static float DISABLE_DISCOUNT_RATE = 0.6f, MERIT_DISCOUNT_RATE = 0.5f,
-			MULTICHILD_DISCOUNT_RATE = 0.8f, PREGNANT_DISCOUNT_RATE = 0.85f;
-	// 최대 주문량
-	final static int MAX_COUNT = 10, MIN_COUNT = 1;
+	final static float 
+	DISABLE_DISCOUNT_RATE = 0.6f, 
+	MERIT_DISCOUNT_RATE = 0.5f,
+	MULTICHILD_DISCOUNT_RATE = 0.8f, 
+	PREGNANT_DISCOUNT_RATE = 0.85f;
+	
 	//종일권 
-	final static String DAY_TICKET = "주간권", NIGHT_TICKET = "야간권";
+	final static 
+	String DAY_TICKET = "주간권", 
+	NIGHT_TICKET = "야간권";
+	
 	//나이별
-	final static String AGE_BABY = "유아", AGE_CHILD = "소인", AGE_TEEN = "청소년", AGE_ADULT = "대인", AGE_OLD = "경로";
+	final static 
+	String AGE_BABY = "유아",
+	AGE_CHILD = "소인", 
+	AGE_TEEN = "청소년", 
+	AGE_ADULT = "대인", 
+	AGE_OLD = "경로";
+	
 	//우대적용
-	final static String NO_DISCOUNT  = "우대적용 없음", DISABLE_DISCOUNT = "장애인 우대적용", MERIT_DISCOUNT = "국가유공자 우대적용" , MULTICHILD_DISCOUNT = "다자녀 우대적용" , PREGNANT_DISCOUNT = "임산부 우대적용";
+	final static 
+	String NO_DISCOUNT  = "우대적용 없음", 
+	DISABLE_DISCOUNT = "장애인 우대적용", 
+	MERIT_DISCOUNT = "국가유공자 우대적용" , 
+	MULTICHILD_DISCOUNT = "다자녀 우대적용" , 
+	PREGNANT_DISCOUNT = "임산부 우대적용";
 }
 
 public int cal_Age () { //1. 입력받은 주민번호에서 생년월일 구하기
@@ -95,7 +123,6 @@ public int cal_Age_Group() { //나이별 종류대로 구분
 
 	
 	public int cal_Price (int ticket_select, int age, int special_rates, int order_amount) {
-//		final int[][] price_array = {{0,0,44000,47000,56000,44000},{0,0,37000,40000,46000,37000}};
 		int cal_price = 0;
 		int a = ticket_select -1;
 		int b = age;
--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
 package Practice;
-
-import java.util.ArrayList;
-
 public class Main {
-public int ticketselect;
-public int agegroup;
-public int specialrates;
-public int ticketamount;
-public int price;
-public int con;
-public int concon;
+
+//Main class에서 사용하는 변수 선언
+private int ticketselect;
+private int agegroup;
+private int specialrates;
+private int ticketamount;
+private int price;
+private int con;
+private int concon;
+private int cnt = 0;
+
 
 public static void main(String[] args){
 	Cal cal = new Cal();
@@ -20,10 +21,12 @@ public static void main(String[] args){
 	
 	do {
 		while(true) {
-		main.ticketselect =Input.input_Ticket_Select();
-		main.agegroup = cal.cal_Age_Group();
-		main.specialrates = Input.input_Special_Rates();
-		main.ticketamount = Input.input_Ticket_Amount();
+		main.cnt++; //주문 카운트
+		
+		main.ticketselect =Input.input_Ticket_Select(); //권종 입력
+		main.agegroup = cal.cal_Age_Group(); //주민번호 입력, 나이 계산
+		main.specialrates = Input.input_Special_Rates(); //우대권 입력
+		main.ticketamount = Input.input_Ticket_Amount(); //수량 입력
 		
 		main.price = (cal.cal_Price(main.ticketselect, main.agegroup, main.specialrates,main.ticketamount));
 		Print.print_price(main.price);
@@ -40,8 +43,9 @@ public static void main(String[] args){
 	
 	print.print_List_Of_Orders();
 	main.concon = input.input_Continue_Continue();
-	} while (main.concon == 1); 	//프로그램 종료인 2가 아니라면 무한 반복
+	} while (main.concon == 1 | main.cnt < 10 ); 	//입력값이 1이거나 주문수가 10보다 작을때까지 무한 반복
 	
-	orderlist.orderList.clear(); //orderlist array 를 초기화
+	main.cnt = 0; //cnt 초기화
+	OrderList.orderList.clear(); //orderlist array 를 초기화
 	}
 }
